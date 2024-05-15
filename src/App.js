3dc4d8e039c91ed8b6cc217e15034c835d47f7b5
unknown <samuel.lekpor@gmail.com>
2024-05-15 17:39:41 +0000

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
-    <div >
-      Phone Directory
+    <div>
+      <Header />
+      <button>Add</button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
